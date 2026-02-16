6426be947ec97cc3e695edba3a7b849679a78a53
Inmaculada Olias <inmaculada.oliasalvarez@iesjacaranda.es>
2026-02-16 09:46:37 +0100

    login

--- src/main/java/com/jacaranda/primeraPrueba/Security/SecurityConfig.java
+++ src/main/java/com/jacaranda/primeraPrueba/Security/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.jacaranda.primeraPrueba.Security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jacaranda.primeraPrueba.exception.ApiError;
+
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebSecurity
@@ -25,22 +35,47 @@ public class SecurityConfig {
 	    return configuration.getAuthenticationManager();
 	}
 	
+	 @Bean
+	    AccessDeniedHandler accessDeniedHandler() {
+	        return (request, response, ex) -> {
+	            response.setStatus(HttpStatus.FORBIDDEN.value());
+	            response.setContentType("application/json");
+
+	            ApiError error = new ApiError(
+	                    HttpStatus.FORBIDDEN.value(),
+	                    HttpStatus.FORBIDDEN.getReasonPhrase(),
+	                    List.of("No tienes permisos para acceder a este recurso"),
+	                    request.getRequestURI()
+	            );
+
+	            new ObjectMapper().writeValue(response.getOutputStream(), error);
+	        };
+	    }
+	
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
 
 		http
+		.cors(cors->{})
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
          
             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .requestMatchers("/register", "/login").permitAll()
-            .anyRequest().authenticated()
+            .requestMatchers(HttpMethod.GET, "/elements/**").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.POST, "/elements/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT, "/elements/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/elements/**").hasRole("ADMIN")
+            .anyRequest().denyAll()
         )
         .exceptionHandling(ex -> ex
         		.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+        		.accessDeniedHandler(accessDeniedHandler())
+
         		);
 
-       
+    	http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 
